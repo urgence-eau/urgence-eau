48d70913115ca730066e39bfdb0c83b8abb9ffcd
tullest <nicolas.venard@epsi.fr>
2020-11-04 16:24:41 +0100

    get input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 window.addEventListener("DOMContentLoaded", event => {
     const btnGeo = document.getElementById("btn-geo");
-    const cordonnee = document.querySelector("#cordonnee");
-
-    console.log(btnGeo, cordonnee);
+    const latitude = document.querySelector("#latitude");
+    const longitude = document.querySelector("#longitude");
+    console.log(btnGeo, latitude, longitude);
 
     btnGeo.addEventListener("click",
         () => navigator.geolocation.getCurrentPosition(
