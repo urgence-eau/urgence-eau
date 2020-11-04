442d4b48c392b9218d150c5c3a7f2ff709c106cd
tullest <nicolas.venard@epsi.fr>
2020-11-04 15:57:22 +0100

    add legend content

--- app.php
+++ app.php
@@ -4,12 +4,34 @@ include_once "header.php";
 $departements = Config::getAllDepartement(1); ?>
 
 <div class="all-container">
-    <div class="map" id="map-div">
-
+    <div class="container-map">
+        <div class="container-legende">
+            <div class="card-legend">
+                <div class="square" style="background-color: #45DE42">
+                </div>
+                <span>0% - 20%</span>
+            </div>
+            <div class="card-legend">
+                <div class="square" style="background-color: #F9E63F">
+                </div>
+                <span>20% - 60%</span>
+            </div>
+            <div class="card-legend">
+                <div class="square" style="background-color: #E8232F">
+                </div>
+                <span>60% - 100%</span>
+            </div>
+            <div class="card-legend">
+                <div class="square" style="background-color: #373435">
+                </div>
+                <span >Non implémenté</span>
+            </div>
+        </div>
+        <div class="map" id="map-div"></div>
     </div>
     <div class="controller-container">
         <div class="info-container">
-            <h4>Témoin d'une fuite ?</h4>
+            <h4 id="title-ita">Témoin d'une fuite ?</h4>
             <span>Signalez la et sauvegardez des milliers de litres d'eau</span>
         </div>
         <button id="btn-incident" data-toggle="modal" data-target=".bd-example-modal-lg">
@@ -21,9 +43,15 @@ $departements = Config::getAllDepartement(1); ?>
                     <h3>Signaler une fuite</h3>
                     <form action="" method="post">
                         <div class="formGroup">
-                            <label for="cordonne">Vos cordonnées</label>
-                            <input id="cordonnee" type="text" name="cordonnee" readonly>
-                            <button id="btn-geo">Je me géolocalise</button>
+                            <label for="latitude">Latitude</label>
+                            <input id="latitude" type="text" name="latitude" readonly>
+                        </div>
+                        <div class="formGroup">
+                            <label for="longitude">Longitude</label>
+                            <input id="longitude" type="text" name="latitude" readonly>
+                        </div>
+                            <button type="button" id="btn-geo">Je me géolocalise</button>
+                        <div class="formGroup">
                             <label for="departement">departement</label>
                             <select name="departement">
                                 <?php foreach ($departements as $departement){ ?>
@@ -32,8 +60,8 @@ $departements = Config::getAllDepartement(1); ?>
                                         </option>
                                 <?php } ?>
                             </select>
-                            <button type="submit">Signaler</button>
                         </div>
+                            <button type="submit">Signaler</button>
                     </form>
                 </div>
             </div>
